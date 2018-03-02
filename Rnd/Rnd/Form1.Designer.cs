9bc9be9e8f187314f0c4d18adb5554660974b52c
Rgb2gray <584125210@qq.com>
2018-03-02 22:33:16 +0800

    Add files via upload

---
+++ Rnd/Rnd/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Rnd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.DownLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btn_test = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.statusStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 84);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 84);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(114, 157);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Arc20\r\nUnbbayes\r\n种子基金\r\n文献mebn\r\ntensorflow";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(184, 139);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Do";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(230, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 20);
+            this.label2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 84);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.DownLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 445);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(712, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // DownLabel
+            // 
+            this.DownLabel.Name = "DownLabel";
+            this.DownLabel.Size = new System.Drawing.Size(131, 17);
+            this.DownLabel.Text = "toolStripStatusLabel1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(315, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 6;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(712, 467);
+            this.tabControl1.TabIndex = 7;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(704, 441);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.btn_test);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(704, 441);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "test";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btn_test
+            // 
+            this.btn_test.Location = new System.Drawing.Point(85, 60);
+            this.btn_test.Name = "btn_test";
+            this.btn_test.Size = new System.Drawing.Size(75, 23);
+            this.btn_test.TabIndex = 0;
+            this.btn_test.Text = "test";
+            this.btn_test.UseVisualStyleBackColor = true;
+            this.btn_test.Click += new System.EventHandler(this.btn_test_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 103);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 175);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 467);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel DownLabel;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btn_test;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Rnd/Rnd/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace Rnd
+{
+    public partial class Form1 : Form
+    {
+        static Dictionary<int, string> planDic = new Dictionary<int, string>();
+        int num_click;
+        string filepath;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = true;
+
+            int rndmax = this.textBox1.Lines.Count() - 1;
+            Console.WriteLine(rndmax);
+            Random rnd = new Random();
+            int num_select = rnd.Next(0, rndmax);
+
+            label2.Text = textBox1.Lines[num_select];
+            Console.WriteLine(num_select);
+
+            DownLabel.Text = "总计：" + rndmax + " " + "选择第" + (num_select + 1) + "项";
+
+            num_click++;
+            label3.Text = "第:" + num_click + "次";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.ReadOnly = false;
+            label2.Text = "";
+
+            num_click = 0;
+        }
+
+        //讲代办事件写入本地txt文件
+        private void Writeplan(string Filepath)
+        {
+            string filepaht = Filepath;
+            StreamWriter sw = new StreamWriter(filepath);
+            for (int i = 0; i < this.textBox1.Lines.Count() - 1; i++)
+            {
+                sw.WriteLine(textBox1.Lines[i]);
+            }
+            //sw.Write(textBox1.Lines[textBox1.Lines.Count() - 1]);
+            sw.Close();
+        }
+
+        //初始化时,txt文件写入textbox
+        private void SetTextbot(string FilePath)
+        {
+            textBox1.Clear();
+            StreamReader reader = new StreamReader(FilePath, false);
+            while (!reader.EndOfStream)
+            {
+                string plan = reader.ReadLine();
+                //this.textBox1.Text += plan + "\r\n";
+                //this.textBox1.AppendText(plan);
+                //this.textBox1.AppendText("\r\n");     
+                this.textBox1.AppendText(plan + Environment.NewLine);
+                
+            }
+            reader.Close();
+
+            //减去textbox最后多的空行            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //设置
+            filepath = Application.StartupPath;
+
+            Directory.SetCurrentDirectory(Directory.GetParent(filepath).FullName);
+            filepath = Directory.GetCurrentDirectory();
+            Directory.SetCurrentDirectory(Directory.GetParent(filepath).FullName);
+            filepath = Directory.GetCurrentDirectory();
+            Directory.SetCurrentDirectory(Directory.GetParent(filepath).FullName);
+            filepath = Directory.GetCurrentDirectory() + @"\plan.txt";
+            // Console.WriteLine(path);
+
+            
+             SetTextbot(filepath);
+             DownLabel.Text = "总计：" + (textBox1.Lines.Count() - 1) + " ";
+
+             num_click = 0;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Writeplan(filepath);
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int k = textBox1.Text.Length;
+            textBox1.Width = k * 4;
+        }
+
+        private void btn_test_Click(object sender, EventArgs e)
+        {
+            int[] testdata = { -2, 2, 1, -5, 3, 0, 4, -2, 2, 1};
+            IList<int> testList = new List<int>();
+            
+
+            foreach (int i in testList)
+            {
+                textBox2.AppendText(i.ToString());
+                textBox2.AppendText("\r\n");
+            }
+            //textBox2.AppendText(testdata[0].ToString());
+
+        }
+    }
+}
